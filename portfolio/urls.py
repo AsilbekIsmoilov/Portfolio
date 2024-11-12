1de3632a3af6c783c4d861bf1e1fcce042b62abd
Asilbek <asilbekismoilov212002@gmail.com>
2024-11-12 15:57:42 +0500

    My Portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from portfolio.views import InfoList, InfoListByCategory
 
 urlpatterns = [
-    path('', InfoList.as_view() ,name='index'),
+    path('', InfoList.as_view(),name='index'),
     path('category/<slug:slug>', InfoListByCategory.as_view() ,name='category'),
 ]
